cd7ec26f8f6a6c69f8dca28956f177d7260a6c8a
Y M <ym@Ys-MBP-2.lan>
2024-07-17 21:04:58 -0700

    Third change

--- employee.py
+++ employee.py
@@ -6,4 +6,18 @@ class Employee:
         self.name = ""
 
     def set_position(self, positionname):
-        self.position = 
+        self.position = positionname
+    
+    def get_position(self, position):
+        return self.position
+    
+    def increment_cars_sold(self):
+        self.cars_sold += self.cars_sold
+
+    def get_cars_sold(self):
+        return self.cars_sold
+    
+    def get_revenue_generate(self):
+        return self.revenue_generated
+    
+    def generate_revenue(self)
