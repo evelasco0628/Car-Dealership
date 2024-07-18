36b5056f0d33deb046a233329513c100126e6dd2
Y M <ym@Ys-MBP-2.lan>
2024-07-17 20:51:32 -0700

    Second Change

--- employee.py
+++ employee.py
@@ -1,1 +1,9 @@
-print("h")
+class Employee:
+    def __init__(self) :
+        self.cars_sold = 0
+        self.revenue_generated = 0.0
+        self.position = ""
+        self.name = ""
+
+    def set_position(self, positionname):
+        self.position = 
