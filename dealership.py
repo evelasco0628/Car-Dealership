039946dd96e65d8ba2df47c1905dc04206c8a0aa
Enrique Velasco <evelasco0628@gmail.com>
2024-07-17 20:56:04 -0700

    Added functions add_car and get_cars to dealership

--- dealership.py
+++ dealership.py
@@ -7,4 +7,18 @@ class Dealership:
     def get_revenue(self):
         return self.revenue
     
-    
+    def add_car(self, model_name, used):
+        value = 0
+        if model_name == "Model1":
+            value = 5000
+        elif model_name == "Model2":
+            value = 10000
+        elif model_name == "Model3":
+            value = 15000
+        else:
+            value = 20000
+
+        car = Car(model_name, value, used)
+
+    def get_cars(self):
+        return self.inventory
