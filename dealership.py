117e779e814eb5eca5ac42dfa9933b7f6f198c22
Enrique Velasco <evelasco0628@gmail.com>
2024-07-17 20:49:42 -0700

    Added dealership class fields and get_revenue function

--- dealership.py
+++ dealership.py
@@ -1,1 +1,10 @@
-print("Test!")
+class Dealership:
+    def __init__(self):
+        self.staff = []
+        self.inventory = []
+        self.revenue = 0
+
+    def get_revenue(self):
+        return self.revenue
+    
+    
